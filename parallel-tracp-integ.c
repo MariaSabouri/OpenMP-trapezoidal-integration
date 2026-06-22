c1f121127e865926e161bf8f1e0c6e2f30331d5e
MariaSabouri <mariasabouri3@gmail.com>
2026-06-22 20:51:47 +0330

    Implement OpenMP trapezoidal integration

--- parallel-tracp-integ.c
+++ parallel-tracp-integ.c
@@ -3,34 +3,39 @@
 #include <stdlib.h>
 #include "math.h"
 
+
+
 #define U(x) sin(x)
 #define a 0
 #define b acos(-1.0)
 
+/*
+If your code includes mathematical functions (like exp, cos, etc.), you need to link to the mathematics 
+library libm.so. This is done, just like for serial compiling, by adding -lm to the end of your compile command, 
+that is,
 
-void distribute_local_values(
-    int my_rank,
-    int thead_count,
-    int n,
-    int* local_n
-){
-    /*calculate local_n*/
-    *local_n = n / thead_count;
-    int remainder = n % thead_count;
-    *local_n += (my_rank < remainder) ? 1 : 0;
-    
-    printf("rank: %d ,local n: %d\n",my_rank, *local_n);
-}
+gcc -fopenmp parallel-tracp-integ.c -o parallel-tracp-integ -lm
+*/
    
 
-void Trap(int n , double* global_result){
-    double h, x, my_result;
-    int my_rank = omp_get_thread_num();
-    int thead_count = omp_get_num_threads();
-    int local_n;
+void Trap(int n , double* global_result, int thead_count){
+    double h, x, my_result, temp_global_result;
+    int i;
 
+    // int my_rank = omp_get_thread_num();
+    // int thead_count = omp_get_num_threads();
+    
     h = (b - a) / n;
-    distribute_local_values(my_rank, thead_count, n, &local_n); 
+    temp_global_result = *global_result;
+
+    #pragma omp parallel for num_threads(thead_count) default(none) reduction(+: temp_global_result) private(i) shared(n, h)
+
+    for (i = 0; i < n; i++) {
+        temp_global_result += U(a + i*h);
+    }
+
+    *global_result = temp_global_result;
+    *global_result *= h;
 
 }
 
@@ -40,7 +45,7 @@ int main(int argc, char* argv[]) {
     int n = strtol(argv[2], NULL, 10);
     double global_result = (U(a) + U(b)) / 2;
 
-    
+    Trap(n, &global_result, thead_count);
 
-
+    printf("area is: %lf\n", global_result);
 }
