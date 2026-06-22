bd0df9942a7b9b8cc5c07127a71268863ed452d9
MariaSabouri <mariasabouri3@gmail.com>
2026-06-22 21:23:24 +0330

    adding a print!

--- parallel-tracp-integ.c
+++ parallel-tracp-integ.c
@@ -31,6 +31,7 @@ void Trap(int n , double* global_result, int thead_count){
     #pragma omp parallel for num_threads(thead_count) default(none) reduction(+: temp_global_result) private(i) shared(n, h)
 
     for (i = 0; i < n; i++) {
+        if (i == 0) printf("number of thead(s) is(are): %d\n", omp_get_num_threads());
         temp_global_result += U(a + i*h);
     }
 
